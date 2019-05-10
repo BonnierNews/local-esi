061e1e39378b4f5644bd099bee77de5d594c1991
Joel Abrahamsson <mail@joelabrahamsson.com>
2019-05-10 09:09:10 +0200

    Refactoring: extract evaluateExpression

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const url = require("url");
 const request = require("request");
 const transformHtml = require("./lib/transformHtml");
-const esiExpressionParser = require("./lib/esiExpressionParser");
+const evaluateExpression = require("./lib/evaluateExpression");
 
 const voidElements = ["area", "base", "br", "col", "embed", "hr", "img", "input", "link", "meta", "param", "source", "track", "wbr"];
 const selfClosingElements = ["esi:include", "esi:eval", "esi:assign", "esi:debug"];
@@ -422,102 +422,6 @@ function ESIListener(context) {
       return next(null, chunk);
     }
     next();
-  }
-}
-
-function evaluateExpression(test, context) {
-  const funcs = {
-    Identifier(node, nodeContext = context.assigns) {
-      return nodeContext[node.name];
-    },
-    exists([arg]) {
-      return !!getFunc(arg.type)(arg);
-    },
-    int([arg]) {
-      return parseInt(getFunc(arg.type)(arg));
-    },
-    index([arg1, arg2]) {
-      return getFunc(arg1.type)(arg1).indexOf(getFunc(arg2.type)(arg2));
-    },
-    // eslint-disable-next-line camelcase
-    base64_decode([arg]) {
-      const string = getFunc(arg.type)(arg);
-      if (!string) {
-        return "";
-      }
-      return Buffer.from(string, "base64").toString("utf8");
-    },
-    // eslint-disable-next-line camelcase
-    base64_encode([arg]) {
-      const string = getFunc(arg.type)(arg);
-      if (!string) {
-        return "";
-      }
-      return Buffer.from(string, "utf8").toString("base64");
-    },
-    CallExpression(node) {
-      return getFunc(node.callee.name)(node.arguments);
-    },
-    LogicalExpression(node) {
-      const left = getFunc(node.left.type);
-      const right = getFunc(node.right.type);
-
-      if (node.operator === "&" || node.operator === "&&") return left(node.left) && right(node.right);
-      if (node.operator === "|" || node.operator === "||") return left(node.left) || right(node.right);
-
-      throw new Error(`Uknown BinaryExpression operator ${node.operator}`);
-    },
-    BinaryExpression(node) {
-      const left = getFunc(node.left.type)(node.left);
-      const right = getFunc(node.right.type)(node.right);
-
-      if (node.operator === "==") return left === right;
-      if (node.operator === ">=") return left >= right;
-      if (node.operator === "<=") return left <= right;
-      if (node.operator === "<") return left < right;
-      if (node.operator === ">") return left > right;
-      if (node.operator === "has") return left.indexOf(right) > -1;
-      if (node.operator === "has_i") return left.toLowerCase().indexOf(right.toLowerCase()) > -1;
-      if (node.operator === "matches") {
-        if (!left) {
-          return;
-        }
-        return left.match(right);
-      }
-      if (node.operator === "matches_i") {
-        if (!left) {
-          return;
-        }
-        return left.match(new RegExp(right, "i"));
-      }
-
-      throw new Error(`Uknown BinaryExpression operator ${node.operator}`);
-    },
-    MemberExpression(node) {
-      const object = getFunc(node.object.type)(node.object);
-
-      if (!object) return;
-
-      return getFunc(node.property.type)(node.property, object);
-    },
-    Literal(node) {
-      return node.value;
-    },
-    UnaryExpression(node) {
-      if (node.operator !== "!") {
-        throw new Error(`Unary operator ${node.operator} not implemented`);
-      }
-
-      return !getFunc(node.argument.type)(node.argument);
-    }
-  };
-
-  const parsedTree = esiExpressionParser(test);
-  return getFunc(parsedTree.type)(parsedTree);
-
-  function getFunc(name) {
-    if (!funcs[name]) throw new Error(`${name} is not implemented`);
-    return funcs[name];
   }
 }
 
